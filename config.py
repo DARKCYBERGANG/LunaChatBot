1d47abc5dc16f02071673f9086c566014f2cb06f
DARKCYBERGANG <71146813+DARKCYBERGANG@users.noreply.github.com>
2021-07-26 11:50:31 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
-ARQ_API_KEY = "Get this from @ARQBot"
+bot_token = "1949888313:AAFyeQfCkBYFPfM3yw5PKY12ZQ9U2XiN39s"
+ARQ_API_KEY = "DMVWEU-YIJYXU-GAVKDF-GVDLRP-ARQ"
 
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
